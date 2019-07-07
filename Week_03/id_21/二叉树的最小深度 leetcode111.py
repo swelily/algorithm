c9b0641f62a88ff51fb166fc360efb6ca14a921c
swelily <50915753+swelily@users.noreply.github.com>
2019-07-07 02:11:50 -0500

    Add files via upload

---
+++ Week_03/id_21/二叉树的最小深度 leetcode111.py
@@ -0,0 +1,25 @@
+class TreeNode(object):
+    # Definition of a binary tree node.
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        children = [root.left, root.right]
+        # if we're at leaf node
+        if not any(children):
+            return 1
+
+        min_depth = float('inf')
+        for c in children:
+            if c:
+                min_depth = min(self.minDepth(c), min_depth)
+        return min_depth + 1
